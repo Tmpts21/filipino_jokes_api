2b7647713f0e436ef46802ea9789e4f0f3a3b8d3
mivatampos@tip.edu.ph <mivatampos@tip.edu.ph>
2021-03-20 22:44:15 +0800

    first commit

---
+++ handler.js
@@ -0,0 +1,66 @@
+const jokes = require('./jokes/validated.json')  
+
+
+const randomJoke = () => { 
+    return jokes[Math.floor(Math.random() * jokes.length)]
+} 
+
+const randomJokeByDialect = (dialect) => { 
+
+    const dialectJokes =  jokes.filter((joke) => {   
+        return joke.dialect === dialect  
+    });     
+
+    if (dialectJokes.length <= 0 ) {   
+        return 'Dialect is not supported. Add more jokes by cotributing to this project'
+    }
+
+    return (dialectJokes[Math.floor(Math.random() * dialectJokes.length)])
+}
+
+
+const nRandomJokes = ( jokesArray = jokes  , n) => {   
+
+    if (!isNumber(n) || n > 10 || n <= 0 ) {  
+        return 'Invalid number of joke :) Jokes should be not greater than 10 or less than 1   ' 
+    }
+
+    randomIndices = new Set();     
+    
+    while (randomIndices.size != n  ) {   
+        randomIndex = Math.floor(Math.random() * jokesArray.length)
+        randomIndices.add(randomIndex)  
+    }    
+ 
+    return Array.from(randomIndices).map((index) => { 
+        return jokesArray[index]
+    })
+
+    }
+
+const nRandomJokesByDialect = (dialect , n = 1 ) =>  {       
+
+
+    if (n > 10 || n <= 0  || !isNumber(n) ) { 
+        return 'Invalid number of joke :) Jokes should be not greater than 10 or less than 1   ' 
+    }
+        
+    const dialectJokes =  jokes.filter((joke) => {   
+        return joke.dialect === dialect 
+    });   
+
+    return nRandomJokes( dialectJokes , n ) 
+
+}
+
+const _10randomJokes = () => {  
+    return nRandomJokes(jokes , 10 ); 
+}  
+
+const isNumber = (n) => {  
+    // checks if the n is a valid integer 
+    return  /^-?[\d.]+(?:e-?\d+)?$/.test(n); 
+} 
+
+
+module.exports = {randomJoke , nRandomJokes , nRandomJokesByDialect , randomJokeByDialect , _10randomJokes}; 
---
+++ server.js
@@ -0,0 +1,67 @@
+const express = require('express');  
+const { reset } = require('nodemon');   
+const rateLimit = require("express-rate-limit"); 
+const app = express() ;          
+
+app.set('trust proxy', 1);
+
+const limiter = rateLimit({
+  windowMs: 30 * 60 * 1000,
+  max: 30 
+});
+
+app.use(limiter);
+app.use(express.static('public'))
+
+const {randomJoke , nRandomJokes, nRandomJokesByDialect , randomJokeByDialect, _10randomJokes} = require('./handler')  
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  next();
+});   
+
+
+app.get('/' , (req , res ) => { 
+     res.send("allowed endpoints [ /jokes , /jokes/ten , /jokes/:number , /jokes/random/:dialect , /jokes/random/:dialect/:number ] ") 
+ });
+
+// grab a random joke 
+app.get('/jokes' , (req , res  )  => {      
+    res.json(randomJoke())
+});  
+
+// grab ten random jokes 
+app.get('/jokes/ten' , ( req , res ) => {       
+    res.json(_10randomJokes());
+});  
+
+// grab random jokes based on number specified in params  
+app.get('/jokes/:number' , (req , res ) => {   
+    // I put undefined in the first param so that it will use the default parameter i specified 
+    res.json(nRandomJokes(undefined , req.params.number))
+});    
+
+// grab random joke based on dialect  
+app.get('/jokes/random/:dialect' , (req , res  )  => {      
+    res.json(randomJokeByDialect(req.params.dialect))
+}); 
+
+// grab random joke based on dialect and number specified in the params   
+app.get('/jokes/random/:dialect/:number' , (req , res  )  => {      
+    res.json(nRandomJokesByDialect(req.params.dialect , req.params.number ))
+
+});    
+
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+
+  res.status(statusCode).json({
+    type: 'error', message: err.message
+  });
+});   
+
+var port = process.env.PORT || 8001;
+
+app.listen(port , () => {   
+    console.log('you are listening to port', `${port} ` )    
+});
